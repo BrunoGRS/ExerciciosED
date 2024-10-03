ca4b4fbad293f1d1a6870265c336da6eb9ef0793
Ezequias <zequicandeia@gmail.com>
2024-10-03 20:48:26 -0300

    .

---
+++ ListaAdjacencia/main.cpp
@@ -0,0 +1,6 @@
+/*
+  criar uma lista com valores usando ponteiros
+  popular a lista com os valores aleatórios
+  posibilitar a união dos valores
+  mostrar a lista com os valores e respectiva união
+*/
